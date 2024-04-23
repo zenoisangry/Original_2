5c478b66d98817e6d895aeab3d869059323a9882
zenoisangry <pedalinz@gmail.com>
2024-04-23 10:58:09 +0200

    Creating Testing Scene in own Scene (ZenoScene)

---
+++ GameLab5_HiddenWorld/Assets/Contents/Scripts/SO_NPC.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SO_NPC : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite npc;
+}
